fac0cba73bddc8bd360c082991e8cba656c1ca7b
wuwj-cn <wenjie.0617@gmail.com>
2017-06-07 23:35:08 +0800

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
 
+/* App Root */
 import { AppComponent } from './app.component';
 
+/* Feature Modules */
+import { CoreModule } from './core/core.module';
+
+/* Routing Module */
+import { AppRoutingModule } from './app-routing.module';
+import { ContactModule } from "app/contact/contact.module";
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
+    ContactModule,
     BrowserModule,
-    FormsModule,
-    HttpModule
+    CoreModule.forRoot({ userName: 'Miss Marple' }),
+    AppRoutingModule
   ],
-  providers: [],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
